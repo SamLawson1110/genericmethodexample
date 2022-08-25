54d7c6e370c2b6e60234349beb6c8d7f5e9c092b
SamLawson1110 <sammaker1110@gmail.com>
2022-08-24 23:57:53 -0400

    added array lists

--- src/GenericMethodClass.java
+++ src/GenericMethodClass.java
@@ -1,7 +1,15 @@
+import java.util.*;
 public class GenericMethodClass {
     public <T> void printArray(T[] array){
         for( T arrayitem : array){
             System.out.println((arrayitem));
         }
     }
+
+    public void printList(List<?> list){
+        for(Object elem : list){
+            System.out.println(elem);
+        }
+    }
+
 }
--- src/GenericMethodDriver.java
+++ src/GenericMethodDriver.java
@@ -1,9 +1,23 @@
+import java.util.*;
+
 public class GenericMethodDriver {
     public void main(String[] args){
         GenericMethodClass gmc = new GenericMethodClass();
 
         Integer[] integerArray = {1, 2, 3};
         String[] stringArray = {"This", "is", "fun"};
+
+        ArrayList<Double> array1 = new ArrayList<>();
+        array1.add(25.5);
+        array1.add(34.9);
+        array1.add(45.7);
+        gmc.printList(array1);
+
+        ArrayList<Integer> array2 = new ArrayList<>();
+        array2.add(10);
+        array2.add(20);
+        array2.add(30);
+        gmc.printList(array2);
 
         gmc.printArray(integerArray);
         gmc.printArray(stringArray);
